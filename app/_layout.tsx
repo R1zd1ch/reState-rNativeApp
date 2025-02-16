247f997c3ee43a141af94e794e2b7ce0e07fddb7
Евгений Червев <evgeniy_cheraev@vk.com>
2025-02-17 02:45:24 +0300

    created

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,30 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+import '../global.css'
+import { SplashScreen, Stack } from 'expo-router'
+import { useFonts } from 'expo-font'
+import { useEffect } from 'react'
+import GlobalProvider from '@/lib/global-provider'
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+	const [fontsLoaded] = useFonts({
+		'Rubik-Bold:': require('../assets/fonts/Rubik-Bold.ttf'),
+		'Rubick-ExtraBold': require('../assets/fonts/Rubik-ExtraBold.ttf'),
+		'Rubick-Light': require('../assets/fonts/Rubik-Light.ttf'),
+		'Rubick-Medium': require('../assets/fonts/Rubik-Medium.ttf'),
+		'Rubick-Regular': require('../assets/fonts/Rubik-Regular.ttf'),
+		'Rubick-SemiBold': require('../assets/fonts/Rubik-SemiBold.ttf'),
+	})
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
+	useEffect(() => {
+		if (fontsLoaded) {
+			SplashScreen.hideAsync()
+		}
+	}, [fontsLoaded])
 
-  if (!loaded) {
-    return null;
-  }
+	if (!fontsLoaded) return null
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+	return (
+		<GlobalProvider>
+			<Stack screenOptions={{ headerShown: false }} />
+		</GlobalProvider>
+	)
 }
